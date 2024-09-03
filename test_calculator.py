f93ce417388dc1409732caf020b3655e2761afe5
abhishekk51 <abhishekthis100@gmail.com>
2024-09-03 16:04:30 +0530

    test case to check different delimeters (RED)

--- test_calculator.py
+++ test_calculator.py
@@ -23,6 +23,10 @@ class TestStringCalculator(unittest.TestCase):
         calc = StringCalculator()
         self.assertEqual(calc.add("1\n2,3"), 6)
 
+    def test_different_delimiters(self):
+        calc = StringCalculator()
+        self.assertEqual(calc.add("//;\n1;2"), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
