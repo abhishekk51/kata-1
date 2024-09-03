99535666866186f69180166691584d49875a20db
abhishekk51 <abhishekthis100@gmail.com>
2024-09-03 16:09:12 +0530

    minimum code to pass different delimeters (GREEN)

--- calculator.py
+++ calculator.py
@@ -1,7 +1,17 @@
+import re
+
+
 class StringCalculator:
     def add(self, numbers):
         if not numbers:
             return 0
-        numbers = numbers.replace("\n", ",")
-        num_list = map(int, numbers.split(","))
+
+        delimiter = ","
+        if numbers.startswith("//"):
+            parts = numbers.split("\n", 1)
+            delimiter = re.escape(parts[0][2:])
+            numbers = parts[1]
+
+        numbers = numbers.replace("\n", delimiter)
+        num_list = map(int, numbers.split(delimiter))
         return sum(num_list)
