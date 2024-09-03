e51f73c11081293f67de1e12d3062eba0ed4ef5a
abhishekk51 <abhishekthis100@gmail.com>
2024-09-03 15:59:45 +0530

    minimum code to pass new lines between numbers test (GREEN)

--- calculator.py
+++ calculator.py
@@ -2,5 +2,6 @@ class StringCalculator:
     def add(self, numbers):
         if not numbers:
             return 0
+        numbers = numbers.replace("\n", ",")
         num_list = map(int, numbers.split(","))
         return sum(num_list)
