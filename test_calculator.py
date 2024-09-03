c7cbd6f385b0d7298861e1152ff5b62c55839a13
abhishekk51 <abhishekthis100@gmail.com>
2024-09-03 16:59:32 +0530

    test case to check multiple delimerter (RED)

--- test_calculator.py
+++ test_calculator.py
@@ -42,6 +42,11 @@ class TestStringCalculator(unittest.TestCase):
         result = calc.add("//[***]\n1***2***3")
         self.assertEqual(result, 6)
 
+    def test_multiple_delimiters(self):
+        calc = StringCalculator()
+        result = calc.add("//[*][%]\n1*2%3")
+        self.assertEqual(result, 6)
+
 
 if __name__ == '__main__':
     unittest.main()
