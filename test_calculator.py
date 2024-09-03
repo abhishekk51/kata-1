e94b4a9071594f672b4fb3a846ceaceb0a666953
abhishekk51 <abhishekthis100@gmail.com>
2024-09-03 14:39:46 +0530

    minimum code to pass blank string test

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,11 @@
 import unittest
+from calculator import StringCalculator
 
 
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string_returns_zero(self):
         calc = StringCalculator()
+        self.assertEqual(calc.add(""), 0)
 
 
 if __name__ == '__main__':
